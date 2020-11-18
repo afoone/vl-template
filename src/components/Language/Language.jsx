0e344379a025cc42903a6439c2bb9599b5f01ef6
Alfonso Tienda <atienda@iprocuratio.com>
2020-11-18 09:40:07 +0100

    language export

--- src/components/Language/Language.jsx
+++ src/components/Language/Language.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export const Language = () => {
     return (
         <div>
-            
+
         </div>
     )
 }
+
+export default Language
--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -1,16 +1,17 @@
 import { Box } from '@material-ui/core'
 import React from 'react'
-import {Logout} from '../Logout/Logout';
-import {MainLogo} from '../MainLogo/MainLogo';
-import {ModulesMainHeader} from '../ModulesMainHeader/ModulesMainHeader';
+import { Logout } from '../Logout/Logout';
+import { MainLogo } from '../MainLogo/MainLogo';
+import { ModulesMainHeader } from '../ModulesMainHeader/ModulesMainHeader';
+import Language from '../Language/Language'
 
 export const MainHeader = () => {
     return (
-        <div className = 'main-header'>
-            <MainLogo/>
-            <ModulesMainHeader/>
-            <Language/>
-            <Logout/>
+        <div className='main-header'>
+            <MainLogo />
+            <ModulesMainHeader />
+            <Language />
+            <Logout />
         </div>
     )
 }
