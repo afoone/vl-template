ecb9e932de06b1983d41091a95e5610be138984f
Alfonso Tienda <afoone@hotmail.com>
2020-12-09 18:11:07 +0100

    menu contextual

--- src/components/tables/ButtonsClickRenderer.jsx
+++ src/components/tables/ButtonsClickRenderer.jsx
@@ -1,16 +1,40 @@
-import { Button } from '@material-ui/core'
-import React from 'react'
+import { Button, Menu, MenuItem } from '@material-ui/core'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next';
 
 
 const ButtonsClickRenderer = ({ context, value }) => {
 
-    const {t} = useTranslation();
-    
+    const { t } = useTranslation();
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const edit = value => {
+        context.edit(value);
+        setAnchorEl(null)
+    }
+
     return (
-        <div>
-            <Button variant="contained" color="primary" size="small"
-                onClick={() => context.edit(value)}>{t("edit")}</Button>
+        <div className="button-cell-renderer">
+            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}
+                variant="contained" color="primary" size="small"
+            >...</Button>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={() => edit(value)}>{t("edit")}</MenuItem>
+            </Menu>
         </div>
     )
 }
